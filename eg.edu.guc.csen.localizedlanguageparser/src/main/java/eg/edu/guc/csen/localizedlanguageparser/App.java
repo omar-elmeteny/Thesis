f9d761d27b234465e756183ab73d92d0e299c07a
Omar El-Meteny <omar.elmeteny@gmail.com>
2023-03-26 22:21:14 +0200

    Created a parser empty project

---
+++ eg.edu.guc.csen.localizedlanguageparser/src/main/java/eg/edu/guc/csen/localizedlanguageparser/App.java
@@ -0,0 +1,13 @@
+package eg.edu.guc.csen.localizedlanguageparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ eg.edu.guc.csen.localizedlanguageparser/src/test/java/eg/edu/guc/csen/localizedlanguageparser/AppTest.java
@@ -0,0 +1,20 @@
+package eg.edu.guc.csen.localizedlanguageparser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
